7083342c931ed564a244a32d5d33b5971c29b4fe
Andres H <None>
2022-09-16 13:52:43 -0500

    Se agrego CORS

--- Parcial 1/ServidorExpress/servidorexpress.js
+++ Parcial 1/ServidorExpress/servidorexpress.js
@@ -1,12 +1,17 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var cors = require('cors')
+
+var app = express()
+
+//app.use(cors({origin: "http://localhost"}))
+app.use(cors({ origin:"*"}))
 
 app.get('/', (req, res) => {
-    res.send('Servidor express constestando - 19100197')
+    res.send('Servidor Express constestando desde Get: PORT8082 - 19100197')
 })
 
 app.post('/', (req, res) => {
-    res.send('Servidor Express Recibio Post')
+    res.send('Servidor Express Recibio Post desde el puerto 8082')
 })
 
 app.listen(8082, () => {
@@ -14,6 +19,17 @@ app.listen(8082, () => {
 })
 
 
+
+
+
+
+
+
 // 1. npm init, para crear los archivos, la carpeta de modulos.
 // 2. npm install express , que este en la carpeta nodule modules, para que nos baje el capeta express
 // 3.- node servidorexpress <- nombre de tu archivo
+
+
+
+// Para instalar el cors: npm install cors
+// C:\xampp\htdocs\hernandezlopezCarlosAndres\Parcial 1\ServidorExpress
