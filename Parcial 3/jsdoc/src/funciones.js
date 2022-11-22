68835bccdea399b09183d0e3c0b5d3bd1711fe8e
Andres H <None>
2022-11-22 08:41:22 -0600

    Se agrego Jsdoc

---
+++ Parcial 3/jsdoc/src/funciones.js
@@ -0,0 +1,11 @@
+/**
+ * Funcion que nos devuelve la potencia de un numero.
+ * @constructor
+ * @param {Int} numbase - El numero base el cual se va a exponenciar.
+ * @param {Int} numexp - El numero exponenciador.
+ */
+
+
+export function potencia(numbase,numexp){
+    return numbase ^ numexp;
+}
